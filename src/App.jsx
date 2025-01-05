5d58e65d652f740182795ca33f708b1fe262f3e5
Swathi <arlapudiswathi03@gmail.com>
2025-01-05 20:02:30 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Example from './Example';
+import Header from './Header.jsx';
+import Example1 from './Example1.jsx';
+
+function App() {
+  return (
+    <><Header/> 
+  <Example1/>
+  </>
+  );
+}
+
+export default App;//jsx example
---
+++ src/Example.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Example(){
+    return<h1>Hello EveryOne</h1>
+}
+
+export default Example;
